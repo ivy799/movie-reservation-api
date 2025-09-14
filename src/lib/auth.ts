c236fed4e4b5ebdb009a6415a705c9b20192167b
ivy799 <muhammadraihanaan123@gmail.com>
2025-09-14 14:21:49 +0800

    FEAT: Integrate Upstash Redis adapter for session management and update dependencies

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,12 @@
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaClient } from "@prisma/client"
+import { UpstashRedisAdapter } from "@auth/upstash-redis-adapter"
 import { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { Redis } from "@upstash/redis"
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+})
 
 declare module "next-auth" {
   interface Session {
@@ -14,48 +19,15 @@ declare module "next-auth" {
   }
 }
 
-const prisma = new PrismaClient()
-
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter: UpstashRedisAdapter(redis),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  session: {
-    strategy: "database",
-  },
   secret: process.env.NEXTAUTH_SECRET,
-  events: {
-    async signIn({ user, account }) {
-      console.log("User login:", user)
-      console.log("Access Token dari Google:", account?.access_token)
-      if (account?.access_token) {
-        await prisma.account.update({
-          where: {
-            provider_providerAccountId: {
-              provider: account.provider,
-              providerAccountId: account.providerAccountId,
-            },
-          },
-          data: { access_token: account.access_token },
-        })
-      }
-    },
-    async signOut({ session }) {
-      if (session?.user?.id) {
-        console.log("Menghapus session untuk user:", session.user.id)
-
-        await prisma.session.deleteMany({
-          where: { userId: session.user.id },
-        })
-
-        console.log("Session berhasil dihapus")
-      }
-    },
-  },
   callbacks: {
     async session({ session, user }) {
       if (session.user) {
